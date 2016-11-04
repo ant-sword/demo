fec24d39f60568e3038f4f2c6b1db6a42fa47acb
ant-sword <i@uyu.us>
2016-11-04 12:02:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,24 +12,42 @@ import {
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      status: 'Loading..'
   }
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello World!</Text>
+        <Text>{this.state.status}</Text>
       </View>
     );
   }
 
   componentDidMount() {
-    const { setBottomBarStyle } = this.props;
-    console.log('setBottomBarStyle=', setBottomBarStyle);
-    window.setBottomBarStyle = setBottomBarStyle;
-    this.props.setBottomBarStyle({
+    const { setBottomBarStyle, regOnBack, regOnAction } = this.props;
+    setBottomBarStyle && setBottomBarStyle({
       color: '#09F',
       borderColor: '#EEE',
       backgroundColor: '#FFF'
-    })
+    });
+    
+    regOnBack && regOnBack(() => {
+      this.setState({
+        status: '点击了返回' + String(+new Date)
+      });
+    });
+    
+    regOnAction && regOnAction(() => {
+      setBottomBarStyle({
+        color: 'red',
+        backgroundColor: '#FFF',
+        borderColor: 'red',
+      });
+      this.setState({
+        status: '点击了菜单'
+      });
+    });
   }
 }
 
